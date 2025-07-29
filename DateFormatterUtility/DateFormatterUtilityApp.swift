5bc98e2a5ddac612abdbc23777882ed38ca9d016
Tahir Anil Oghan <t.anil.oghan@gmail.com>
2025-07-29 04:30:06 +0300

    Initial Commit

---
+++ DateFormatterUtility/DateFormatterUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateFormatterUtilityApp.swift
+//  DateFormatterUtility
+//
+//  Created by Tahir Anil Oghan on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DateFormatterUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DateFormatterUtilityTests/DateFormatterUtilityTests.swift
@@ -0,0 +1,17 @@
+//
+//  DateFormatterUtilityTests.swift
+//  DateFormatterUtilityTests
+//
+//  Created by Tahir Anil Oghan on 29.07.2025.
+//
+
+import Testing
+@testable import DateFormatterUtility
+
+struct DateFormatterUtilityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
